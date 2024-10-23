1e68c1514c628292f49d418c959dd7784bfff2f1
EduardV1 <a23eduvilvil@inspedralbes.cat>
2024-10-23 13:15:11 +0200

    revisió a casa

--- back/takeAway-back/database/migrations/2024_10_23_084250_create_tables.php
+++ back/takeAway-back/database/migrations/2024_10_23_084250_create_tables.php
@@ -4,24 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+return new class extends Migration {
+
+
+    public function up()
     {
-        Schema::create('tables', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::create("products", function (Blueprint $table) {
+            $table->double("id");
+            $table->string("title");
+            $table->string("description");
+            $table->string("image");
+            $table->string("price");
+            $table->string("category_id");
+            $table->string("size_id");
         });
+
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('tables');
+        Schema::dropIfExists("products");
     }
+
 };

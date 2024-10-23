ae9e7752f5ad4b4a52e63e780da2157222f634ff
aleiram19 <a23aleminram@inspedralbes.cat>
2024-10-23 09:05:55 +0200

    avance3_getJson

---
+++ back/getImage.php
@@ -0,0 +1,17 @@
+<?php
+
+$json = file_get_contents('data.json');
+$productsArray = json_decode ($json, true);
+
+foreach($productsArray['products'] as $product) {
+    echo "ID: ".$product['id']."<br>" ;
+    echo "Title: ".$product['title']."<br>";
+    echo "Description: ".$product['description']."<br>";
+    echo "<img src='" .$product['image']. "' style= 'width: 200px;'> <br>";
+    echo "Price: ".$product['price']." € <br>";
+    echo "Category: ".$product['category']."<br>";
+    echo "Size: ".$product['size']."<br><br>";
+}
+
+
+?> 

79ccb34dcc88c38ffd2b1df77d7e918172299c48
EduardV1 <a23eduvilvil@inspedralbes.cat>
2024-10-24 08:35:50 +0200

    BBDD creada, i amb les relacions de Foreign Key Fetes.

--- back/takeAway-back/resources/views/getImage.blade.php
+++ back/takeAway-back/resources/views/getImage.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Llistat Productes</title>
+    <title>Llistat de Productes</title>
 </head>
 
 <style>
@@ -17,12 +17,12 @@
     @foreach($products as $product)
         <div>
             <h2>ID: {{ $product['id'] }}</h2>
-            <h3>Título: {{ $product['title'] }}</h3>
-            <p>Descripción: {{ $product['description'] }}</p>
+            <h3>Títol: {{ $product['title'] }}</h3>
+            <p>Descripció: {{ $product['description'] }}</p>
             <img src="{{ $product['image'] }}">
-            <p>Precio: {{ $product['price'] }} €</p>
-            <p>Categoría: {{ $product['category'] }}</p>
-            <p>Tamaño: {{ $product['size'] }}</p>
+            <p>Preu: {{ $product['price'] }} €</p>
+            <p>Categoria: {{ $product['category'] }}</p>
+            <p>Talla: {{ $product['size'] }}</p>
         </div>
         <br>
     @endforeach

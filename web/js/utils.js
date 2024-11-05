0876c842ce7ba30dd38b70bc55fce099b5f2734f
aleiram19 <a23aleminram@inspedralbes.cat>
2024-11-05 10:43:10 +0100

    Cambios de NavBar y Añadir Formularios LOGIN

--- web/js/utils.js
+++ web/js/utils.js
@@ -3,7 +3,7 @@ import { createApp, reactive, ref, onBeforeMount } from 'https://unpkg.com/vue@3
 
 createApp({
     setup() {
-        let laravel = reactive ({ URL: "http://localhost:8000"})
+        let laravel = reactive({ URL: "http://localhost:8000" })
         let templateData = reactive({ products: [] });
         //let visible = ref('store');
         let selectedProduct = reactive([]);
@@ -12,22 +12,22 @@ createApp({
         let visible = ref('page-cover');
         const visibleButtons = ref('');
         let cartVisible = ref(false);
-
-
+        let showForm = ref(false);
+        
         function subTotalCart() {
             let total = 0;
             for (let i = 0; i < this.cartItems.length; i++) {
                 total += this.cartItems[i].price * this.cartItems[i].quantity;
             }
-            
+
             return total.toFixed(2);
         }
 
 
         function showCartFloat() {
-            if (this.visible === 'store' || this.visible === 'productSelec'){
+            if (this.visible === 'store' || this.visible === 'productSelec') {
                 this.cartVisible = true;
-            }else {
+            } else {
                 this.cartVisible = false;
             }
         }
@@ -97,7 +97,7 @@ createApp({
             cartItems.splice(0, cartItems.length)
         }
 
-        
+
 
         function addToCart(product) {
 
@@ -147,8 +147,58 @@ createApp({
             }
         });
 
+        function continuePurchase() {
+            visible.value = 'purchase-form'; // Muestra el formulario al continuar
+        }
+
+        function submitForm() {
+            console.log("Formulario enviado");
+            visible.value = 'purchase-completed'; ; // Vuelve a la vista de compra completada
+            cleanCart();
+        }
+
+
+        onBeforeMount(async () => {
+            try {
+                const result = await getData();
+                templateData.products = result;
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        });
+
+        // FUNCIONES DE LOGIN Y REGISTER
+
+        function showLogin() {
+            visible.value = 'login-form';
+        }
+
+        function showRegister() {
+            visible.value = 'register-form';
+        }
+
+        function backToPurchaseForm() {
+            visible.value = 'purchase-form'; // Para regresar al formulario de compra
+        } 
+
+        function login() {
+            console.log('Iniciar sesión con:', {
+                email: document.getElementById('login-email').value,
+                password: document.getElementById('login-password').value
+            });
+        }
+
+        function register() {
+            console.log('Registrar con:', {
+                name: document.getElementById('reg-name').value,
+                surname: document.getElementById('reg-surname').value,
+                phone: document.getElementById('reg-phone').value,
+                email: document.getElementById('reg-email').value,
+                password: document.getElementById('reg-password').value
+            });
+        }
         return {
-            templateData, changeDiv, visible, selectedProduct, showSelectedProduct, cartItems, addToCart, objectsInCart, cleanCart, laravel, deleteItemCart, cancelPurchase, visibleButtons, discountProduct, incrementProduct, itemCartEmpty, cartVisible, showCartFloat,subTotalCart
+            templateData, changeDiv, visible, selectedProduct, showSelectedProduct, cartItems, addToCart, objectsInCart, cleanCart, laravel, deleteItemCart, cancelPurchase, visibleButtons, discountProduct, incrementProduct, itemCartEmpty, cartVisible, showCartFloat, subTotalCart, continuePurchase, showForm, submitForm, showLogin, showRegister, backToPurchaseForm, login, register 
         };
     }
 }).mount('#app');

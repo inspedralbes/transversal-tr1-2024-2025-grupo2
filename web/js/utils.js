1ecf31f4fdc5abbd665318e8934822514d39d1d0
LucasBenitez10 <a23lucbensoj@inspedralbes.cat>
2024-11-06 10:26:57 +0100

    Revert "Merge 2 branch 'product-list' of https://github.com/inspedralbes/transversal-tr1-2024-2025-grupo2_takeAway into product-list"
    This reverts commit 75b46e373d38ded765d95ada7a91000304f05393, reversing
    changes made to 8c3c2e26e74adb021e953f8669967bad4f67e243.

--- web/js/utils.js
+++ web/js/utils.js
@@ -1,9 +1,9 @@
 import { getData } from './communicationManager.js'
-import { createApp, reactive, ref, onBeforeMount,watch } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+import { createApp, reactive, ref, onBeforeMount, watch } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
 createApp({
     setup() {
-        let laravel = reactive({ URL: "http://localhost:8000" })
+        let laravel = reactive ({ URL: "http://localhost:8000"})
         let templateData = reactive({ products: [] });
         //let visible = ref('store');
         let selectedProduct = reactive([]);
@@ -12,7 +12,6 @@ createApp({
         let visible = ref('page-cover');
         const visibleButtons = ref('');
         let cartVisible = ref(false);
-        let showForm = ref(false);
         const isMenuOpen = ref(false);
 
 
@@ -26,21 +25,22 @@ createApp({
                 document.body.classList.remove('menu-open');
             }
         });
-        
+
+
         function subTotalCart() {
             let total = 0;
             for (let i = 0; i < this.cartItems.length; i++) {
                 total += this.cartItems[i].price * this.cartItems[i].quantity;
             }
-
+            
             return total.toFixed(2);
         }
 
 
         function showCartFloat() {
-            if (this.visible === 'store' || this.visible === 'productSelec') {
+            if (this.visible === 'store' || this.visible === 'productSelec'){
                 this.cartVisible = true;
-            } else {
+            }else {
                 this.cartVisible = false;
             }
         }
@@ -110,7 +110,7 @@ createApp({
             cartItems.splice(0, cartItems.length)
         }
 
-
+        
 
         function addToCart(product) {
 
@@ -158,61 +158,10 @@ createApp({
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
-        })
+        });
 
-        function continuePurchase() {
-            this.visible = 'purchase-form'; // Muestra el formulario al continuar
-        }
-
-        function submitForm() {
-            console.log("Formulario enviado");
-            visible.value = 'purchase-completed'; // Vuelve a la vista de compra completada
-            cleanCart();
-        }
-
-
-        onBeforeMount(async () => {
-            try {
-                const result = await getData();
-                templateData.products = result;
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        })
-
-        // FUNCIONES DE LOGIN Y REGISTER
-
-        function showLogin() {
-            visible.value = 'login-form';
-        }
-
-        function showRegister() {
-            visible.value = 'register-form';
-        }
-
-        function backToPurchaseForm() {
-            visible.value = 'purchase-form'; // Para regresar al formulario de compra
-        } 
-
-        function login() {
-            console.log('Iniciar sesión con:', {
-                email: document.getElementById('login-email').value,
-                password: document.getElementById('login-password').value
-            });
-        }
-
-        function register() {
-            console.log('Registrar con:', {
-                name: document.getElementById('reg-name').value,
-                surname: document.getElementById('reg-surname').value,
-                phone: document.getElementById('reg-phone').value,
-                email: document.getElementById('reg-email').value,
-                password: document.getElementById('reg-password').value
-            })
-        }
-        
         return {
-            templateData, changeDiv, visible, selectedProduct, showSelectedProduct, cartItems, addToCart, objectsInCart, cleanCart, laravel, deleteItemCart, cancelPurchase, visibleButtons, discountProduct, incrementProduct, itemCartEmpty, cartVisible, showCartFloat, subTotalCart, continuePurchase, showForm, submitForm, showLogin, showRegister, backToPurchaseForm, login, register,isMenuOpen
+            templateData, changeDiv, visible, selectedProduct, showSelectedProduct, cartItems, addToCart, objectsInCart, cleanCart, laravel, deleteItemCart, cancelPurchase, visibleButtons, discountProduct, incrementProduct, itemCartEmpty, cartVisible, showCartFloat,subTotalCart,isMenuOpen
         };
     }
 }).mount('#app');
